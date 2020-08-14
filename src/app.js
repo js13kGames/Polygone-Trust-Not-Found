eb18ea1affdd1cb1bf679af22aa6411d90f29235
André Jaenisch <andre.jaenisch@posteo.de>
2020-08-14 19:43:13 +0200

    Add Device Sensor permissions and readings
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- src/app.js
+++ src/app.js
@@ -33,4 +33,128 @@ class World {
   }
 }
 
+// TODO: Assign this to different children of #app
+// Modify World class to pass in the property to change. Perhaps a mapping of event to properties?
+// Allow for callbacks?
 new World(document.querySelector('#app rect'), 50)
+
+/* Feature detection of Sensors API */
+if (typeof Gyroscope === 'function') {
+}
+
+if ('ProximitySensor' in window) {
+}
+
+if (window.Accelerometer) {
+  console.log('Asking for permission to accelerometer')
+  navigator.permissions.query({ name: 'accelerometer' })
+           .then((result) => {
+             if (result.state === 'denied') {
+             console.log('Permission to use accelerometer sensor is denied.')
+             return
+           }
+           console.log('Permission to use accelerometer sensor is granted.')
+
+           let acl = new Accelerometer({ frequency: 60 })
+
+           acl.addEventListener('reading', () => {
+             console.log('Acceleration along the X-axis ' + acl.x)
+             console.log('Acceleration along the Y-axis ' + acl.y)
+             console.log('Acceleration along the Z-axis ' + acl.z)
+           })
+
+           acl.start()
+           console.log('ACL', acl)
+  })
+} else {
+  console.info('No Accelerometer available')
+}
+
+if (window.AmbientLightSensor) {
+  console.log('Asking for permission to ambient light sensor')
+  navigator.permissions.query({ name: 'ambient-light-sensor' })
+           .then((result) => {
+             if (result.state === 'denied') {
+             console.log('Permission to use ambient-light-sensor sensor is denied.')
+             return
+           }
+           console.log('Permission to use ambient-light-sensor sensor is granted.')
+
+           let sensor = new AmbientLightSensor()
+           sensor.onreading = () => {
+             console.log('Current light level: ', sensor.illuminance)
+           }
+           sensor.onerror = (event) => {
+             console.error(event.error.name, event.error.message)
+           }
+           sensor.start()
+           console.log('Ambient Light sensor', sensor)
+  })
+} else {
+  console.info('No AmbientLightSensor available')
+}
+
+if (window.Gyroscope) {
+  console.log('Asking for permission to gyroscope')
+  navigator.permissions.query({ name: 'gyroscope' })
+           .then((result) => {
+             if (result.state === 'denied') {
+             console.log('Permission to use gyroscope sensor is denied.')
+             return
+           }
+           console.log('Permission to use gyroscope sensor is granted.')
+
+           let gyroscope = new Gyroscope({ frequency: 60 })
+
+           gyroscope.addEventListener('reading', () => {
+             console.log('Angular velocity along the X-axis ' + gyroscope.x)
+             console.log('Angular velocity along the Y-axis ' + gyroscope.y)
+             console.log('Angular velocity along the Z-axis ' + gyroscope.z)
+           })
+
+           gyroscope.start()
+           console.log('Gyroscope', gyroscope)
+  })
+} else {
+  console.info('No Gyroscope available')
+}
+
+if (window.Magnetometer) {
+  console.log('Asking for permission to magnetometer')
+  navigator.permissions.query({ name: 'magnetometer' })
+           .then((result) => {
+             if (result.state === 'denied') {
+             console.log('Permission to use magnetometer sensor is denied.')
+             return
+           }
+           console.log('Permission to use magnetometer sensor is granted.')
+
+           let magnetometer = new Magnetometer({ frequency: 60 })
+
+           magnetometer.addEventListener('reading', () => {
+             console.log('Magnetic field along the X-axis ' + magnetometer.x)
+             console.log('Magnetic field along the Y-axis ' + magnetometer.y)
+             console.log('Magnetic field along the Z-axis ' + magnetometer.z)
+           })
+
+           magnetometer.start()
+           console.log('Magnetometer', magnetometer)
+  })
+} else {
+  console.info('No Magnetometer available')
+}
+
+/* Sensors have the fields:
+ * activated: Boolean
+ * hasReading: Boolean
+ * timestamp
+ *
+ * Sensors have the events:
+ * reading
+ * error
+ * activated
+ *
+ * Sensors have the methods:
+ * start()
+ * stop()
+ */
