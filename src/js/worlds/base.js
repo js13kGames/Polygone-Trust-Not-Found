78e0b06b3f40c013f56cd7311a0fd655fdadd33d
André Jaenisch <andre.jaenisch@posteo.de>
2020-09-08 21:25:31 +0200

    Document BaseWorld
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- src/js/worlds/base.js
+++ src/js/worlds/base.js
@@ -1,25 +1,58 @@
 import { WithParent } from '../mixins/with-parent'
 
+/**
+ * The base class for shared logic of worlds.
+ * @extends WithParent
+ */
 class BaseWorld extends WithParent {
+  /** Used as identifier for transitions between worlds. */
   static worldName = 'base'
 
+  /**
+   * @param {PropertiesWithParent}
+   */
   constructor (properties) {
     super(properties)
+    /**
+     * Holds the melody to be played while visiting this world.
+     * @type {Array<Number|undefined>}
+     * @todo Abstract away Numbers to musical notes (= strings).
+     */
     this.melody = []
+    /**
+     * Flag to indicate, whether this world is shown in the UI.
+     */
     this._isActive = false
     this._updateView()
   }
 
+  /**
+   * Add elements which are present in this world.
+   * To easier allow for separation in my mind, I tend to split it up in
+   * three chunks. SVG doesn't care however.
+   * @public
+   */
   addScene () {
     this._addBackground()
     this._addMiddleground()
     this._addForeground()
   }
 
+  /**
+   * Is this world currently shown?
+   * @public
+   * @returns {Boolean}
+   */
   isActive () {
     return this._isActive
   }
 
+  /**
+   * Inspired by Maxim (but can't used `with` blocks in strict mode.
+   * @todo Check against a setting to control volume.
+   * @todo Move baseFrequency and timeOfInterval into a constant each.
+   * @todo Abstract musical notes away.
+   */
   playMusic () {
     if (this.melody.length > 0) {
       /* Taken from https://xem.github.io/miniMusic/simple.html */
@@ -47,39 +80,70 @@ class BaseWorld extends WithParent {
     }
   }
 
+  /**
+   * Show this world.
+   * @public
+   */
   setActive () {
     this._isActive = true
     this._updateView()
     this.playMusic()
   }
 
+  /**
+   * Hide this world.
+   * @public
+   */
   setInactive () {
     this._isActive = false
     this._updateView()
   }
 
+  /**
+   * @protected
+   */
   _addBackground () {
     // To be implemented in sub-classes
   }
 
+  /**
+   * @protected
+   */
   _addForeground () {
     // To be implemented in sub-classes
   }
 
+  /**
+   * @protected
+   */
   _addMiddleground () {
     // To be implemented in sub-classes
   }
 
+  /**
+   * Add a new element to the DOM.
+   * @param {HTMLElement} parent
+   */
   _mount (parent) {
     const classes = ['world', 'world--' + this.constructor.worldName]
     this.element = this._createSvgElement('g', {}, classes)
     parent.appendChild(this.element)
   }
 
+  /**
+   * Meant to randomise certain elements of the world.
+   * @protected
+   * @param {Number} [min=1] - Lower boundary.
+   * @param {Number} [max=6] - Upper boundary.
+   * @returns {Number}
+   */
   _rollDice (min = 1, max = 6) {
     return Math.floor(Math.random() * ((max - min) + min) + min)
   }
 
+  /**
+   * Updates UI of this element.
+   */
   _updateView () {
     super._updateView()
     if (this.isActive()) {
