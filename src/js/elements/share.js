08e046b90aa091eee86e5f873fb4da1fae40cdec
André Jaenisch <andre.jaenisch@posteo.de>
2020-09-13 07:33:00 +0200

    Add share button
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- src/js/elements/share.js
+++ src/js/elements/share.js
@@ -1,3 +1,4 @@
+import { EVENTS } from '../constants'
 import { t } from '../translations'
 
 import { WithParent } from '../mixins/with-parent'
@@ -7,6 +8,32 @@ import { WithParent } from '../mixins/with-parent'
  * @extends WithParent
  */
 class Share extends WithParent {
+  /**
+   * @param {PropertiesWithParent} properties
+   */
+  constructor (properties) {
+    super(properties)
+
+    /**
+     * The text to share on Twitter.
+     * @private
+     * @todo Make translateable
+     */
+    this.__linkText= `${t('SHARE_TITLE')} by @AndreJaenisch`
+    this._updateView()
+  }
+
+  /**
+   * Listen to time updates.
+   * @protected
+   * @returns {{}}
+   */
+  _getEventMap () {
+    return {
+      [ EVENTS.TICK ]: this.__handleGameTimeUpdate.bind(this)
+    }
+  }
+
   /**
    * Adds a new element to the DOM.
    * @protected
@@ -23,26 +50,62 @@ class Share extends WithParent {
   }
 
   /**
-   * Adds the share link.
-   * @private
-   * @see {@link https://developer.twitter.com/en/docs/twitter-for-websites/tweet-button/overview}
+   * Updates the UI
+   * @protected
    */
-  __mountShare () {
-    const linkTitle = t('LINK_TITLE')
-    const tweet = encodeURIComponent(`${linkTitle} by @AndreJaenisch`)
+  _updateView () {
+    const link = this.element.querySelector('.share__link')
+    link.setAttribute('href', this.__getHref())
+  }
+
+  /**
+   * Constructs the href of the share link.
+   * @private
+   * @returns {String}
+   */
+  __getHref () {
+    const linkText = this.__linkText
+      ? `${this.__linkText} by @AndreJaenisch` 
+      : ''
+    const tweet = encodeURIComponent(linkText)
     const hashtags = encodeURIComponent([
       'js13k',
       'polygone'
     ].join(','))
     const url = encodeURIComponent(location.href)
-    const href = `https://twitter.com/intent/tweet?text=${tweet}&hashtags=${hashtags}&url=${url}`
+    return `https://twitter.com/intent/tweet?text=${tweet}&hashtags=${hashtags}&url=${url}`
+  }
 
+  /**
+   * Update the link text
+   * @private
+   * @param {{}}     clock
+   * @param {Number} clock.day
+   * @param {Number} clock.hour
+   * @param {Number} clock.minute
+   * @todo Make translateable
+   */
+  __handleGameTimeUpdate (clock) {
+    const day = clock.day === 1 ? '1 day' : `${clock.day} days`
+    const hour = ('00' + clock.hour).slice(-2)
+    const minute = ('00' + clock.minute).slice(-2)
+    this.__linkText = `I played »Polygone - Trust Not Found« for ${day}, ${hour}:${minute} by @AndreJaenisch`
+    this._updateView()
+  }
+
+  /**
+   * Adds the share link.
+   * @private
+   * @see {@link https://developer.twitter.com/en/docs/twitter-for-websites/tweet-button/overview}
+   */
+  __mountShare () {
+    const href = this.__getHref()
     const link = this._createHtmlElement(
       'a',
       { href },
       [ 'share__link' ]
     )
-    const text = document.createTextNode(linkTitle)
+    const text = document.createTextNode(t('LINK_TITLE'))
     link.appendChild(text)
     this.element.appendChild(link)
   }
